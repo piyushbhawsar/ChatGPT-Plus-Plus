06c3979843bf673310483a66ffc44b678c44a401
piyushbhawsar <111758968+piyushbhawsar@users.noreply.github.com>
2023-05-19 06:22:35 +0530

    Add .gitignore file

--- README.md
+++
@@ -1,6 +0,0 @@
-chatbot capable of human text 
-
-- general purpose
-- coding expert
-- poetry generator
-- acads assistance
--- env.js
+++
@@ -1,5 +0,0 @@
-export const process = {
-    env: {
-        OPENAI_API_KEY: "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"
-    }
-}
